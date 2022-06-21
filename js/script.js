c9fb5a678a00d585ebbc16a319bcb6590870fd5c
KOVU9529 <bapgiordano@gmail.com>
2022-06-21 15:31:55 +0200

    inizio commenti

---
+++ js/script.js
@@ -0,0 +1,148 @@
+/*Ciao Ragazzi,
+Esercizio di oggi:
+cartella/repo js-es6-icons
+Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+BONUS
+1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+2- popolare le options della select della milestone 3 dinamicamente.*/
+
+//Dati Forniti
+const icon=[
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+//Milestone 1
+//Partendo dalla struttura dati fornita, 
+//creare una variabile di riferimneto al container fisico(html)
+//creare un ciclo forEach(per far scorrere la creazione id ogni nuovo box)
+//visualizzare in pagina un box per ogni icona, 
+//Creare un template che corrisponda al box che conterrà l'icona e il proprio nome
+//I dati per riempire il box saranno gli oggetti dell'array<i class="family: 'fas',prefix: 'fa-',name: 'horse',"></i>
+//in cui è presente il nome dell'icona e l'icona stessa.
+
+//Milestone 2
+//Ciascuna icona ha una proprietà "color": 
+//Attribuire ad ogni incona il riferimento al proprio colore ESEMPIO(color: 'blue')=> verrà aggiunta come classe all'icona
+//utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+//Milestone 3
+//Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal,
+// vegetable, user). 
+//CReare un select dove le opzioni sono gli oggetti  ( type: 'user',)
+//Quando l'utente seleziona un tipo dalla select, 
+//visualizzare solamente le icone corrispondenti.
